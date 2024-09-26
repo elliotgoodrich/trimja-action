6d880bc62cf99cf93e7ffe9b2a02e1c418f69536
Elliot Goodrich <elliotgoodrich@gmail.com>
2024-09-26 06:03:33 +0100

    Add a post build step
    Add a placeholder post build step that runs only on success.
    
    In this post build step we will cache the `.ninja_deps` and `.ninja_log`
    if the rest of the job is successful.
    
    Move the temporary files to the `$builddir` and only include the final
    bundles JavaScript to `dist`.

--- ninjutsu.mjs
+++ ninjutsu.mjs
@@ -1,8 +1,9 @@
-import { NinjaBuilder } from "@ninjutsu-build/core";
+import { NinjaBuilder, getInput } from "@ninjutsu-build/core";
 import { makeTSCRule } from "@ninjutsu-build/tsc";
 import { makeFormatRule, makeLintRule } from "@ninjutsu-build/biome";
 import { makeESBuildRule } from "@ninjutsu-build/esbuild";
 import { writeFileSync } from "node:fs";
+import { join, parse } from "node:path/posix";
 
 const biomeConfig = "biome.json";
 
@@ -24,46 +25,48 @@ format({ in: "ninjutsu.mjs" });
 format({ in: "package.json" });
 format({ in: "biome.json" });
 
-const formatted = format({ in: "index.mts" });
-const linted = lint({ in: formatted });
+for (const source of ["main.mts", "post.mts"]) {
+  const formatted = format({ in: source });
+  const linted = lint({ in: formatted });
 
-const sources = tsc({
-  in: [linted],
-  compilerOptions: {
-    target: "ES2018",
-    lib: ["ES2021"],
-    outDir: "dist",
-    module: "nodenext",
-    moduleResolution: "nodenext",
-    esModuleInterop: true,
-    forceConsistentCasingInFileNames: true,
-    strict: true,
-    noImplicitAny: true,
-    strictNullChecks: true,
-    strictFunctionTypes: true,
-    strictBindCallApply: true,
-    strictPropertyInitialization: true,
-    noImplicitThis: true,
-    useUnknownInCatchVariables: true,
-    alwaysStrict: true,
-    noUnusedLocals: true,
-    noUnusedParameters: true,
-    noImplicitReturns: true,
-    noFallthroughCasesInSwitch: true,
-    skipDefaultLibCheck: true,
-    skipLibCheck: true,
-    isolatedModules: true,
-  },
-});
+  const [generatedJS] = tsc({
+    in: [linted],
+    compilerOptions: {
+      target: "ES2018",
+      lib: ["ES2021"],
+      outDir: "$builddir",
+      module: "nodenext",
+      moduleResolution: "nodenext",
+      esModuleInterop: true,
+      forceConsistentCasingInFileNames: true,
+      strict: true,
+      noImplicitAny: true,
+      strictNullChecks: true,
+      strictFunctionTypes: true,
+      strictBindCallApply: true,
+      strictPropertyInitialization: true,
+      noImplicitThis: true,
+      useUnknownInCatchVariables: true,
+      alwaysStrict: true,
+      noUnusedLocals: true,
+      noUnusedParameters: true,
+      noImplicitReturns: true,
+      noFallthroughCasesInSwitch: true,
+      skipDefaultLibCheck: true,
+      skipLibCheck: true,
+      isolatedModules: true,
+    },
+  });
 
-esbuild({
-  in: sources,
-  out: "dist/entry.cjs",
-  buildOptions: {
-    bundle: true,
-    platform: "node",
-    target: "es2022",
-  },
-});
+  esbuild({
+    in: generatedJS,
+    out: join("dist", `${parse(getInput(generatedJS)).name}.cjs`),
+    buildOptions: {
+      bundle: true,
+      platform: "node",
+      target: "es2022",
+    },
+  });
+}
 
 writeFileSync("build.ninja", ninja.output);
